e9af02c5ca4990159853274fdbebbe61de01886b
contattiflowstudio-web <contattiflowstudio@gmail.com>
2026-05-04 19:38:29 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename = "/ironclad">
+      <BrowserRouter basename = "/pulse09">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
