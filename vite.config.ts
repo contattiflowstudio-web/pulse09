a1cf9426bc1f4974205903e0f9dddbbd4078c78a
contattiflowstudio-web <contattiflowstudio@gmail.com>
2026-05-04 19:38:12 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/ironclad", 
+  base: "/pulse09", 
   
   server: {
     host: "::",
